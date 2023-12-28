8ad91c424560e143b3ac7f6662eefb409c831494
suranol <aira.suranol02@gmail.com>
2023-12-28 19:27:34 +0800

    Added movie details page

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const show_upcoming_movies = async () =>{
     const response = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=1bfdbff05c2698dc917dd28c08d41096&language=en-US&page=1");
     const data = await response.json();
     container.innerHTML = data.results.map((data)=>(
-        `<button>
+        `<button ondblclick=show_movie_details(${data.id})>
         <img src=${imageUrl + data.backdrop_path}>
         <h3>${data.original_title}</h3>
         </button>`
@@ -32,6 +32,29 @@ const search_movies = async (name) =>{
         </button>`
     ))
 }
+
+const show_movie_details = async (id) =>{
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=1bfdbff05c2698dc917dd28c08d41096&language=en-US`);
+    const data = await response.json();
+
+    const similar_movies = await fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=1bfdbff05c2698dc917dd28c08d41096&language=en-US&page=1`)
+    const similar_movies_data = await similar_movies.json()
+
+    container.innerHTML = 
+    `<div>
+    <h1>Title: ${data.original_title}</h1>
+    <span>Overview: ${data.overview}</span> 
+    <div>
+    <h1>Similar Movies  </h1>
+    ${similar_movies_data.results.map((data)=>(`<button>
+        <img src=${imageUrl + data.backdrop_path}>
+        <h3>${data.original_title}</h3>
+        </button>`
+    ))}
+    </div>
+     </div> `;
+}
+
 
 
 
