9a72a8932a98bd4907a24874d92d649f14d2e6e3
suranol <aira.suranol02@gmail.com>
2023-12-30 16:41:27 +0800

    Add background

--- script.js
+++ script.js
@@ -11,16 +11,19 @@ form.addEventListener("submit",(e)=>{
 
 })
 
-const show_upcoming_movies = async () =>{
+const show_upcoming_movies = async () => {
     const response = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=1bfdbff05c2698dc917dd28c08d41096&language=en-US&page=1");
     const data = await response.json();
-    container.innerHTML = data.results.map((data)=>(
-        `<button ondblclick=show_movie_details(${data.id})>
-        <img src=${imageUrl + data.backdrop_path}>
-        <h3>${data.original_title}</h3>
-        </button>`
-    ))
-}
+    container.innerHTML = data.results.map((movie) => (
+        `<div class="movie-item" ondblclick="show_movie_details(${movie.id})">
+            <img class="movie-poster" src="${imageUrl + movie.backdrop_path}" alt="${movie.original_title}">
+            <h3 class="movie-title">${movie.original_title}</h3>
+            <button class="details-button" onclick="movie-poster(${movie.id})"></button>
+        </div>`
+    )).join('');
+};
+
+
 
 const search_movies = async (name) =>{
     const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=1bfdbff05c2698dc917dd28c08d41096&query=${name}`);
@@ -41,9 +44,18 @@ const show_movie_details = async (id) =>{
     const similar_movies_data = await similar_movies.json()
 
     container.innerHTML = 
-    `<div>
+    `<div class="details">
     <h1>Title: ${data.original_title}</h1>
     <span>Overview: ${data.overview}</span> 
+    <span>Genres ${data.genres.map((data) =>(data.name)).join(" ")}
+    <span>Popularity ${data.popularity}</span>
+    <span>Release Date ${data.release_date}</span>  
+    <span>Spoken Languages ${data.spoken_languages.map((data) =>(data.english_name)).join(" ")}
+    <span>Vote Average ${data.vote_average}</span> 
+
+
+
+    
     <div>
     <h1>Similar Movies  </h1>
     ${similar_movies_data.results.map((data)=>(`<button>
@@ -52,7 +64,7 @@ const show_movie_details = async (id) =>{
         </button>`
     ))}
     </div>
-     </div> `;
+     </div> `
 }
 
 
